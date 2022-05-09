64c8595c2bb16aab64c5d5dd3dc526788238a438
Yashar <yashare@gmail.com>
2022-05-09 11:50:59 -0400

    Cleared test lines, added bit of types

--- Wordle/wordle.py
+++ Wordle/wordle.py
@@ -41,26 +41,22 @@ def letter_hit(word_list, letter, i):
     return [x for x in word_list if x[i] == letter]
 
 
-
+# TODO: Fix the scoring to remove the found letters from calculations
 def sort_by_score(word_list):
     cnt = letter_count(word_list)
     word_list.sort(key=lambda x: sum([cnt[s] for s in x]), reverse=True)
     return word_list
 
 
-def set_result(used, response):
+def set_result(used: str, response: str) -> [dict]:
+    '''
+    
+    '''
     result = list(enumerate(zip(used, response)))
     result = [{'index': x[0], 'letter': x[1][0], 'response': x[1][1]} for x in result] 
     return result
 
-# used = input('Word used:')
-# result = input('Result: ')
-#%%
 
-
-# used = 'aisle'
-# response = 'ymmmg'
-#%%
 if __name__ == '__main__':
     wordle_words = read_words()
     print('Here are the first suggestions to start the game:')
@@ -69,7 +65,14 @@ if __name__ == '__main__':
     while turns < 6:
         used = input('Word played: ')
         turns += 1
-        response = input('Response: ')
+        chk = True
+        while chk:
+            response = input('Response: ')
+            if (len(response) != 5) | (set(response) != set('gmy')):
+                print('Check the response!')
+            else:
+                chk = False
+        
         if response == 'ggggg':
             print('Nice!')
             break
@@ -79,7 +82,7 @@ if __name__ == '__main__':
                 wordle_words = remove_letter(wordle_words, item['letter'])
             elif item['response'] == 'g':
                 wordle_words = letter_hit(wordle_words, item['letter'], item['index'])
-            else:
+            elif item['response'] == 'y':
                 wordle_words = letter_found(wordle_words, item['letter'], item['index'])
         print(sort_by_score(wordle_words)[:10])
     if turns > 6:
