9c0282364525d28084b778c8d84f40c5afba0da9
Yashar <yashare@gmail.com>
2022-08-30 14:10:39 -0400

    Separation of word list creation applied to api

--- Wordle/wordle_api.py
+++ Wordle/wordle_api.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-from typing import Optional
 from pydantic import BaseModel, Field
-from wordle_classbased import Wordle
+from wordle_classbased import Wordle, create_word_list
 
 app = FastAPI()
 
@@ -10,12 +9,12 @@ class Response(BaseModel):
     word_played: str = Field(regex=r'^[a-z]{5}$')
     answer: str = Field(regex=r'^[gmy]{5}$')
 
-wordle = Wordle()
+wordle = Wordle(create_word_list())
 game = {1: wordle}
 
 @app.get("/newgame")
 def newgame():
-    game[1] = Wordle()
+    game[1] = Wordle(create_word_list())
     return {"Suggestions": game[1].wordle_words[:10], "Turn": game[1].turn}
 
 
