15c61d23d84685fcdafc94738a4593c5ef51de94
Yashar <yashare@gmail.com>
2022-08-31 16:17:55 -0400

    Added folder selection - separated functions

--- Clean Film Names.py
+++ Clean Film Names.py
@@ -7,29 +7,44 @@ Created on Fri Jun 19 01:14:05 2020
 
 import os
 import re
-os.chdir('D:/Downloads/')
+from tkinter import filedialog
 
-def clean(f):
-    rem = r'720p|BluRay|x264|YIFY|juggs|\[ETRG\]|HDRip|\[YTS.AG\]|dvdrip|xvid|ETRG|HDTV|\
+
+REMOVE_LIST = r'720p|BluRay|x264|YIFY|juggs|\[ETRG\]|HDRip|\[YTS.AG\]|dvdrip|xvid|ETRG|HDTV|\
         |H264|AC3|xvid|aac|brrip|\[|\]|\{|\}|saas|mp3|rarbg|web-dl|1080p|dvdscr|tehmovies|\
             teh-music|mkvcage| Hc|ganool|-Evo|bdrip|teh-music|nex1movie|shaanig|\
         Korsub|webrip|aqos|S4A|-sam|blu-ray| biz|480p|Mkvhub|Film2Movie|Galaxyrg|\
-            Tehtmovies|- Com|Iran-film|30Nama|Wb-Dl|x265|2Ch|10Bit|Psa'
-    f = re.sub(rem, '', f, flags=re.IGNORECASE)
-    f = re.sub('\.', ' ', f, flags=re.IGNORECASE)
-    f = re.sub('_', ' ', f, flags=re.IGNORECASE)
-    f = re.sub('  ', ' ', f, flags=re.IGNORECASE)
-    f = f.strip().title()
-    f = f.strip('-')
-    f = re.sub('file', 'changed', f, flags=re.IGNORECASE)
-    return f.strip().title()
+            Tehtmovies|- Com|Iran-film|30Nama|Wb-Dl|x265|2Ch|10Bit|Psa|Pahe'
+INITIAL_DIR = 'D:/Downloads/'
 
-mypath = list(reversed(list(os.walk('./'))))
 
-for item in mypath:
-    for file_name in item[2]:
-        f_name, f_ext = os.path.splitext(file_name)
-        os.rename(os.path.join(item[0], file_name), os.path.join(item[0], clean(f_name)+f_ext))
-    if len(item[1])>0:
-        for dir_name in item[1]:
-            os.rename(os.path.join(item[0], dir_name), os.path.join(item[0], clean(dir_name)))
+def remove_extra_words(file_name: str, remove_list: str = REMOVE_LIST) -> str:
+    file_name = re.sub(remove_list, '', file_name, flags=re.IGNORECASE)
+    file_name = re.sub('\.', ' ', file_name, flags=re.IGNORECASE)
+    file_name = re.sub('_', ' ', file_name, flags=re.IGNORECASE)
+    file_name = re.sub('  ', ' ', file_name, flags=re.IGNORECASE)
+    file_name = file_name.strip().title()
+    file_name = file_name.strip('-')
+    file_name = re.sub('file', 'changed', file_name, flags=re.IGNORECASE)
+    return file_name.strip().title()
+
+
+def clean_directory(folder_path: str) -> None:
+    mypath = list(reversed(list(os.walk(folder_path))))
+    for item in mypath:
+        for file_name in item[2]:
+            f_name, f_ext = os.path.splitext(file_name)
+            os.rename(os.path.join(item[0], file_name), os.path.join(item[0], remove_extra_words(f_name)+f_ext))
+        if len(item[1])>0:
+            for dir_name in item[1]:
+                os.rename(os.path.join(item[0], dir_name), os.path.join(item[0], remove_extra_words(dir_name)))
+
+
+def main() -> None:
+    folder_path = filedialog.askdirectory(title='Select directory', initialdir=INITIAL_DIR)
+    if len(folder_path) > 0:
+        clean_directory(folder_path)
+
+
+if __name__ == '__main__':
+    main()
